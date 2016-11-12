8960298380acd66d82c36bdabeb8e72cd6dc84f1
Kevin Mahoney <kjmahoney13@gmail.com>
2016-11-12 14:52:02 -0500

    created profile page renders

--- index.js
+++ index.js
@@ -7,9 +7,22 @@ var seedData = require("./seed.js")
 
 app.get("/", (req, res) => {
   var user = seedData.Users[0]
-  // seedData.seed();
   res.render("index", {user: user})
 })
+
+app.get("/:id", (req, res) => {
+  var customers = seedData.Users
+  for (i = 0; i<customers.length; i++){
+    if (req.params.id == customers[i].id){
+      var name = customers[i].firstName
+    }
+  }
+  res.render("user", {name: name})
+})
+
+//loop Users
+//id match params id
+//print info on page
 
 
 app.set("view engine", "hbs")
--- seed.js
+++ seed.js
@@ -1,6 +1,4 @@
-
-
-var Users = ["Kevin"]
+var Users = []
 
 class User{
   constructor(firstName, lastName, homeAddress, workAddress, phone, id){
@@ -9,16 +7,19 @@ class User{
     this.homeAddress = homeAddress,
     this.workAddress = workAddress,
     this.phone = phone,
-    this.id = id
+    this.id = id,
+    this.vehicle = []
   }
 }
 
 
-const Kevin = new User("Kevin", "Mahoney", "229 G Street SW", "228 North Arlington", 1234567890, 1)
 
-// var seed = function(){
-//   console.log(Users)
-// }
+var seed = function(){
+  const Kevin = new User("Kevin", "Mahoney", "229 G Street SW", "228 North Arlington", 1234567890, 1)
+  const Khoi = new User("Khoi", "Le", "123 H Street NW", "229 North Arlington", 1234567891, 2)
+  Users.push(Kevin, Khoi)
+}
+seed()
 module.exports = {
 Users: Users
 }
