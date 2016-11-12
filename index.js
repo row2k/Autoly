ee0f0c10e2d08225a7a58d9943415fae2849b469
Kevin Mahoney <kjmahoney13@gmail.com>
2016-11-12 17:42:22 -0500

    added car list functionality

--- index.js
+++ index.js
@@ -3,31 +3,48 @@ var app = express();
 var hbs = require("hbs")
 var seedData = require("./seed.js")
 
-
+app.use(express.static("public"))
 
 app.get("/", (req, res) => {
   var user = seedData.Users[0]
+  console.log(user.vehicle)
   res.render("index", {user: user})
 })
 
 app.get("/contactDriver", (req, res) => {
   var drivers = seedData.Drivers
   var randDriver = drivers[Math.floor(Math.random() * drivers.length)]
+  console.log(randDriver.img_url)
   // var rando = Math.floor(Math.random() * drivers.length)
   res.render("driver", {randDriver: randDriver})
 })
 
+app.get("/scheduling", (req, res) => {
+  var drivers = seedData.Drivers
+  var randDriver = drivers[Math.floor(Math.random() * drivers.length)]
+  console.log(randDriver.img_url)
+  // var rando = Math.floor(Math.random() * drivers.length)
+  res.render("driver", {randDriver: randDriver})
+})
+
+//Displays user profiles and their cars
 app.get("/:id", (req, res) => {
   var customers = seedData.Users
   for (i = 0; i<customers.length; i++){
     if (req.params.id == customers[i].id){
       var name = customers[i].firstName
     }
+    for (p = 0; p <customers[i].vehicle.length; p++){
+      var carMake = customers[i].vehicle[p].make
+      var carModel = customers[i].vehicle[p].model
+      var carColor = customers[i].vehicle[p].color
+      var carPlate = customers[i].vehicle[p].plateID
+      var carState = customers[i].vehicle[p].plateState
+    }
   }
-  res.render("user", {name: name})
-})
-
-
+  res.render("user", {name: name, carMake:carMake, carModel:carModel,
+    carColor:carColor, carPlate:carPlate, carState})
+  })
 
 
 app.set("view engine", "hbs")
--- seed.js
+++ seed.js
@@ -11,31 +11,52 @@ class User{
     this.id = id,
     this.vehicle = []
   }
+  newCar(car){
+    this.vehicle.push(car)
+  }
+
+  // newCar(make, model, color, plateID, plateState){
+  //   var car2 = new Vehicle(make, model, color, plateID, plateState)
+  //   this.vehicle.push(car2)
+
+
 }
 class Driver{
-  constructor(firstName, lastName, profile_pic, id){
+  constructor(firstName, lastName, img_url, id){
     this.firstName = firstName,
-    this.lastName = lastName
-    this.profile_pic = profile_pic
+    this.lastName = lastName,
+    this.img_url = img_url,
     this.id = id
 
   }
 }
 
+class Vehicle{
+  constructor(make, model, color, plateID, plateState){
+    this.make = make,
+    this.model = model,
+    this.color = color,
+    this.plateID = plateID,
+    this.plateState = plateState
+  }
+}
 
 
 var seed = function(){
   const Kevin = new User("Kevin", "Mahoney", "229 G Street SW", "228 North Arlington", 1234567890, 1)
   const Khoi = new User("Khoi", "Le", "123 H Street NW", "229 North Arlington", 1234567891, 2)
+
+  const Accord = new Vehicle("honda", "accord", "red","1234567", "VA")
+  Kevin.newCar(Accord)
   Users.push(Kevin, Khoi)
 
   const Jay = new Driver("Jay", "Corleone", "https://v.cdn.vine.co/r/avatars/A9A28624571289204044023345152_4d7d328af95.5.0.jpg?versionId=SR7_.t9s6hbVupWkYtWeAssU84xErvhl", 1 )
-  const Thomas = new Driver ("Thomas", "Smith", "http://beijingcream.com/wp-content/uploads/2013/06/Edward-Snowden-zoomed-in.jpg", 2)
+  const Thomas = new Driver ("Thomas", "Smith", "https://lh4.googleusercontent.com/-iMBIAcdmRPs/UcseOYiTWtI/AAAAAAAAAGo/5Aq8-QwldHo/w1024-h614/good-bye-love.jpg", 2)
   Drivers.push(Jay, Thomas)
 
 }
 seed()
 module.exports = {
 Users: Users,
-Drivers: Drivers
+Drivers: Drivers,
 }
